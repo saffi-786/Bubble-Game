1b23e93bb0558896be01e3165975135f5823c376
Saffi <sksaffiulla786@gmail.com>
2023-08-13 00:53:45 +0530

    Added correct and wrong click audio effects

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
 var timer = 60;
 var score = 0;
 var hitrn = 0;
+var correctBubble = new Audio("right.mp3");
+var wrongBubble = new Audio("wrong.mp3");
 
-function increaseScore(){
-    score += 10;
-    document.querySelector('#scoreVal').textContent = score;
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreVal").textContent = score;
 }
 
-function getNewHit(){
-    hitrn = Math.floor(Math.random()*10);
-    document.querySelector('#hitVal').textContent = hitrn;
+function getNewHit() {
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitVal").textContent = hitrn;
 }
 
-function makeBubble(){
-    var clutter = '';
-    
-    for(var i = 0; i< 112; i++) {
-        clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`;
+function makeBubble() {
+  var clutter = "";
+
+  for (var i = 0; i < 112; i++) {
+    clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`;
+  }
+
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+  var timeRunner = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerCount").textContent = timer;
+    } else {
+      clearInterval(timeRunner);
+      document.querySelector(
+        "#pbtm"
+      ).innerHTML = `<h1 style="color: rgb(90, 126, 90);">GAME OVER</h1>`;
     }
-    
-    document.querySelector('#pbtm').innerHTML = clutter;
+  }, 1000);
 }
 
-function runTimer(){
-    var timeRunner = setInterval(function(){
-        if(timer > 0){
-            timer--;
-            document.querySelector('#timerCount').textContent = timer;
-        }
-        else{
-            clearInterval(timeRunner);
-            document.querySelector('#pbtm').innerHTML = `<h1 style="color: rgb(90, 126, 90);">GAME OVER</h1>`
-        }
-    }, 1000)
-}
+document.querySelector("#pbtm").addEventListener("click", function (details) {
+  var currentHit = Number(details.target.textContent);
 
-document.querySelector('#pbtm').addEventListener('click', function(details){
-    var sound = new Audio('drop.mp3');
-    sound.play();
-    
-    var currentHit = Number(details.target.textContent);
-
-    if(currentHit === hitrn) {
-        increaseScore();
-        getNewHit();
-        makeBubble();
-    }
-})
+  if (currentHit === hitrn) {
+    correctBubble.play();
+    increaseScore();
+    getNewHit();
+    makeBubble();
+  } else {
+    wrongBubble.play();
+  }
+});
 
 runTimer();
 makeBubble();
